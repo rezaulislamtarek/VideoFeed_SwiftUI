dc87dbd1e9c46fdc6b094d4a53582bc3cd36dfbe
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-05-29 12:13:16 +0600

    Initial Commit

---
+++ VideoFeed_SwiftUI/VideoFeed_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeed_SwiftUIApp.swift
+//  VideoFeed_SwiftUI
+//
+//  Created by Rezaul Islam on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeed_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
